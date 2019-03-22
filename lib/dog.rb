f9b305670d6d9cadc0a5a29a67518f6b4fbb9ade
Alberto Morales <cmtmorales24@gmail.com>
2019-03-22 14:38:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class Dog 
+  attr_accessor :name 
+  @@all=[]
+  
+  def initialize(name)
+    @name=name 
+    @@all<< name
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  def self.all
+    @@all.each {|print_pup_names|
+     puts "#{print_pup_names}"
+     }
+  end
+     
+end 
